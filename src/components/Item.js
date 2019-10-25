527756e1fcb89fc827117b1c79ce392c19d0cc8f
Stewart Granger Flores <stewart.grngrf@gmail.com>
2019-10-25 19:00:55 -0300

    Implementando Formatting

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 // envolví data en {} para destructurar el prop que envié desde APP
-//ESTE ERA EL PUTO ERROR DE MIERDA, ME DESPIDO A LAS 5AM REACT BASURA ASQUEROSA.
 function Item({ data }) {
   //paso data a una variable dentro del componente
   const item = data;
 
-  if (item.length > 0) {
+  if (item.length > 1) {
     return (
       <div className="card-group">
         <div className="row">
           {/* recorro mapeando cada item  */}
           {item.map((detalle, index) => {
-            // let time = moment(detalle.global.latest_time, "MM-DD-YYYY");
+            let hora = new Date(detalle.global.latest_time);
+            let precioFormateado =
+              detalle.global.latest
+                .toString()
+                .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + " Zeny";
             return (
               <div key={index} className="col-sm-6 col-lg-3 py-2">
                 <div className="card border-info mb3">
@@ -20,12 +23,13 @@ function Item({ data }) {
                     {/* debido al map no necesito el index porque hara la acción N veces hasta el fin del largo del arreglo */}
                     <h5 className="card-title">{detalle.name}</h5>
                     <p className="card-text text-dark">
-                      Precio : {detalle.global.latest}
+                      Precio : {precioFormateado}
                     </p>
                   </div>
                   <div className="card-footer">
                     <small className="text-warning">
-                      Last updated : {detalle.global.latest_time}
+                      Última Actualización :{" "}
+                      {hora.toLocaleString().slice(0, -3)}
                     </small>
                   </div>
                 </div>
@@ -37,43 +41,44 @@ function Item({ data }) {
     );
   } //fin if
   else {
-    return item.map((detalle, index) => {
-      return (
-        <div className="card-group">
-          <div className="row">
-            <div key={index} className="col-sm-6 col-lg-3 py-2">
-              <div className="card">
-                <div className="card-body">
-                  {/* debido al map no necesito el index porque hara la acción N veces hasta el fin del largo del arreglo */}
-                  <h5 className="card-title">{detalle.nombre}</h5>
-                  <p className="card-text">Precio : {detalle.precio}</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">Last updated 3 mins ago</small>
+    return (
+      <div className="card-group">
+        <div className="row">
+          {/* recorro mapeando cada item  */}
+          {item.map((detalle, index) => {
+            let hora = new Date(detalle.global.latest_time);
+            let precioFormateado =
+              detalle.global.latest
+                .toString()
+                .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + " Zeny";
+            return (
+              <div
+                key={index}
+                className="col-sm-6 col-lg-3 py-2"
+                style={{ minWidth: "100%" }}
+              >
+                <div className="card border-info mb3">
+                  <div className="card-body text-info">
+                    {/* debido al map no necesito el index porque hara la acción N veces hasta el fin del largo del arreglo */}
+                    <h5 className="card-title">{detalle.name}</h5>
+                    <p className="card-text text-dark">
+                      Precio : {precioFormateado}
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <small className="text-warning">
+                      Última Actualización :{" "}
+                      {hora.toLocaleString().slice(0, -3)}
+                    </small>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
-      );
-    });
+      </div>
+    );
   }
 }
 
 export default Item;
-// {item.map((detalle, index) => {
-//   return (
-//     <div className="card">
-//       <img src="..." className="card-img-top" alt="..."/ />
-//       <div className="card-body">
-//         <h5 key={index} className="card-title">
-//           {detalle.nombre}
-//         </h5>
-//       </div>
-//       <div className="card-footer">
-//         <small className="text-muted">Last updated 3 mins ago</small>
-//       </div>
-//     </div>
-
-//   );
-// })}
