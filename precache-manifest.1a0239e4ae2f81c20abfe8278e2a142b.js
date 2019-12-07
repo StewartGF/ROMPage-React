2c5e7987bfe0731c2dc676b9ac8e03419fb2abf7
Stewart Granger Flores <stewart.grngrf@gmail.com>
2019-12-07 20:15:31 -0300

    Updates

---
+++ precache-manifest.1a0239e4ae2f81c20abfe8278e2a142b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef35be8bfc09f8bf67ef1abe116c721",
+    "url": "/ROMPage-React/index.html"
+  },
+  {
+    "revision": "f7ed7fd2543387f1aeef",
+    "url": "/ROMPage-React/static/css/main.67b955ec.chunk.css"
+  },
+  {
+    "revision": "0628a0551dec338a32c4",
+    "url": "/ROMPage-React/static/js/2.daa4f548.chunk.js"
+  },
+  {
+    "revision": "f7ed7fd2543387f1aeef",
+    "url": "/ROMPage-React/static/js/main.81ffc2fe.chunk.js"
+  },
+  {
+    "revision": "c0c0d85edce9ff57a279",
+    "url": "/ROMPage-React/static/js/runtime-main.35a8698c.js"
+  }
+]);
